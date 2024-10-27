2f35aa4f48254d76b8d83e531468fdbb6077905f
HajdukSanchez <hajduksanchez.dev@gmail.com>
2024-10-26 19:22:19 -0500

    Initial Commit

---
+++ Touchdown/TouchdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouchdownApp.swift
+//  Touchdown
+//
+//  Created by Jozek Andrzej Hajduk Sanchez on 26/10/24.
+//
+
+import SwiftUI
+
+@main
+struct TouchdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
